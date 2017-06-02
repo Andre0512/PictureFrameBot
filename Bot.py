f2ccede869db0caa53e334db17218461103e32ea
Andre <andre.basche@gmail.com>
2017-06-02 20:55:46 +0200

    Prototype of a Telegram Bot

---
+++ Bot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    update.message.reply_text('Hi!')
+
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    updater = Updater("TOKEN")
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
