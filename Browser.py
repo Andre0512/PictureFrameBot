95452b0d21393a4af67856600198322fd7bd4907
Andre <andre.basche@gmail.com>
2017-06-05 23:22:00 +0200

    Read html as file, code in functions

--- Browser.py
+++ Browser.py
@@ -1,62 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
 import webbrowser
 import subprocess
 import os
 
 
-test_file = 'test.html'
+def insert_images(html):
+    image_list = []
+    link_list = ["https://ifsstech.files.wordpress.com/2008/10/169.jpg",
+                 "https://designshack.net/wp-content/uploads/16-9.jpg",
+                 "https://upload.wikimedia.org/wikipedia/commons/7/7c/Aspect_ratio_16_9_example.jpg"]
+    delay_time = 1000
+    for image in link_list:
+        image_list.append('<img class ="mySlides" src="' + image + '" style="width:100%">')
 
-f = open(test_file, 'w')
+    html = html.replace("@IMAGES", '\n'.join(image_list))
+    html = html.replace("@DELAY", str(delay_time))
+    return html
 
-message = """
-<!DOCTYPE html>
-<html>
-<title>W3.CSS</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.mySlides {display:none;}
-body { margin:0; }
-</style>
-<body>
-<div class="images" style="width:100%, height:100%">
-  <img class="mySlides" src="https://ifsstech.files.wordpress.com/2008/10/169.jpg" style="width:100%">
-  <img class="mySlides" src="https://designshack.net/wp-content/uploads/16-9.jpg" style="width:100%">
-  <img class="mySlides" src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Aspect_ratio_16_9_example.jpg" style="width:100%">
-</div>
 
-<script>
-var myIndex = 0;
-carousel();
+def write_file(html, html_out_file):
+    out_file = open(html_out_file, 'w')
+    out_file.write(html)
+    out_file.close()
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 200); // Change image every 2 seconds
-}
-</script>
 
-</body>
-</html>
-"""
+def read_file(html_in_file):
+    in_file = open(html_in_file, 'r', encoding='utf-8')
+    html = in_file.read()
+    return html
 
-f.write(message)
-f.close()
 
-fscreen = "./Fullscreen"
-first = False
-if not os.path.isfile(fscreen):
-    first = True
-    os.environ["DISPLAY"] = ":0"
+def open_linux(html_file):
+    fscreen = "./Fullscreen"
+    first = False
+    if not os.path.isfile(fscreen):
+        first = True
+        os.environ["DISPLAY"] = ":0"
 
-webbrowser.register('midori', None, webbrowser.BackgroundBrowser('/usr/bin/midori'))
-browser = webbrowser.get('midori')
-browser.open(test_file)
-if first:
-    subprocess.call('xte "key F11" -x:0', shell=True)
-    open(fscreen,'a').close()
+    webbrowser.register('midori', None, webbrowser.BackgroundBrowser('/usr/bin/midori'))
+    browser = webbrowser.get('midori')
+    browser.open(html_file)
+    if first:
+        subprocess.call('xte "key F11" -x:0', shell=True)
+        open(fscreen, 'a').close()
 
+
+def main():
+    html_in_file = 'std_form.html'
+    html_out_file = 'test.html'
+
+    html = read_file(html_in_file)
+    html = insert_images(html)
+    write_file(html, html_out_file)
+
+    if not os.name == 'nt':
+        open_linux(html_out_file)
+    else:
+        webbrowser.get('windows-default').open(html_out_file)
+
+
+if __name__ == '__main__':
+    main()
