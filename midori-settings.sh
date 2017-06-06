d137d3b551b13f4973fd37640cf8b27473fa4bd0
Andre <andre.basche@gmail.com>
2017-06-06 19:23:12 +0200

    Added script for midori settings

---
+++ midori-settings.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+setting=$(cat << 'EOF' 
+[settings]
+default-encoding=ISO-8859-1
+enable-site-specific-quirks=true
+last-window-width=1920
+last-window-height=1080
+last-panel-position=254
+load-on-startup=MIDORI_STARTUP_HOMEPAGE
+homepage=file:///home/pi/Picture-Frame-Bot/startpage.html
+location-entry-search=https://duckduckgo.com/?q=%s
+open-new-pages-in=MIDORI_NEW_PAGE_CURRENT
+open-tabs-next-to-current=false
+enable-spell-checking=false
+enable-html5-database=true
+user-agent=Mozilla/5.0 (Macintosh; U; Intel Mac OS X; de-de) AppleWebKit/535+ (KHTML, like Gecko) Version/5.0 Safari/535.22+ Midori/0.4
+EOF
+)
+echo "$setting"
